cb6566ac47276d36fc6792e929123f481011c7fc
nvercher <seizieme.nord@gmail.com>
2022-08-14 02:45:44 +0200

    Create 5 round game and declare winner functions

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
-function getComputerChoice() {
-    const options = ["rock", "paper", "scissors"];
-    return options[Math.floor(Math.random() * options.length)];
-}
+let score = 0;
 
-const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-
-const computerSelection = getComputerChoice();
-
-function playRound(playerSelection, computerSelection) {
-    switch(playerSelection + computerSelection) {
-        case "paperpaper":
-            return "You tied! You both picked paper"
-            break;
-        case "rockrock":
-            return "You tied! You both picked rock"
-            break;
-        case "scissorsscissors":
-            return "You tied! You both picked scissors"
-            break;
-        case "paperrock":
-            return "BINGO! Machine had Rock, YOU WIN!"
-            break;
-        case "rockpaper":
-            return "OOPS! Machine had Paper, YOU LOSE!"
-            break;
-        case "paperscissors":
-            return "BOOPS! Machine had Scissors, YOU LOSE!"
-            break;
-        case "scissorspaper":
-            return "BINGO! Machine had Scissors, YOU WIN!"
-            break;
-        case "rockscissors":
-            return "BINGO! Machine had Scissors, YOU WIN!"
-            break;
-        case "scissorsrock":
-            return "OOPS! Machine had Rock, YOU LOSE!"
-            break;
+function game() {
+    for (let i = 0; i < 5; i++) {
+        function getComputerChoice() {
+            const options = ["rock", "paper", "scissors"];
+            return options[Math.floor(Math.random() * options.length)];
+        }
+        
+        const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+        
+        const computerSelection = getComputerChoice();
+        
+        function playRound(playerSelection, computerSelection) {
+            switch(playerSelection + computerSelection) {
+                case "paperpaper":
+                    return "You tied! You both picked paper";
+                    break;
+                case "rockrock":
+                    return "You tied! You both picked rock";
+                    break;
+                case "scissorsscissors":
+                    return "You tied! You both picked scissors";
+                    break;
+                case "paperrock":
+                    score++;
+                    return "BINGO! Machine had Rock, YOU WIN!";
+                    break;
+                case "rockpaper":
+                    return "OOPS! Machine had Paper, YOU LOSE!";
+                    break;
+                case "paperscissors":
+                    return "BOOPS! Machine had Scissors, YOU LOSE!";
+                    break;
+                case "scissorspaper":
+                    score++;
+                    return "BINGO! Machine had Scissors, YOU WIN!";
+                    break;
+                case "rockscissors":
+                    score++;
+                    return "BINGO! Machine had Scissors, YOU WIN!";
+                    break;
+                case "scissorsrock":
+                    return "OOPS! Machine had Rock, YOU LOSE!";
+                    break;
+            }
+        }
+        console.log(playRound(playerSelection, computerSelection));
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+game();
+
+function declareWinner() {
+    if (score > 2) {
+        console.log("Player wins")
+    } else {
+        console.log("Computer wins");
+    }
+} 
+
+declareWinner();
